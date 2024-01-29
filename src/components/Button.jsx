c00afddfec67e0c4b83d3a088e84199a59bb3b5b
Guillermo <zlzline@gmail.com>
2024-01-29 16:55:23 -0300

    Text colors

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,14 @@ import { Children } from "react"
 
 function Button({onClick, Children}) {
     return (
-        <button onClick={onClick} className="btn btn-danger rounded-xl border border-[--accent-200] w-fit p-2 hover:bg-[--accent-200] " > {Children} </button>
-    )
+      <button
+        onClick={onClick}
+        className="btn btn-danger rounded-xl border border-[--accent-200] w-fit p-2 hover:bg-[--accent-200] hover:text-black "
+      >
+        {" "}
+        {Children}{" "}
+      </button>
+    );
 }
 
 export default Button
--- src/components/Remove.jsx
+++ src/components/Remove.jsx
@@ -1,7 +1,13 @@
 function Remove({onClick}) {
     return (
-        <button onClick={onClick} className="btn btn-danger rounded-xl border border-[--accent-200] w-fit p-2 hover:bg-[--accent-200] " > Remove </button>
-    )
+      <button
+        onClick={onClick}
+        className="btn btn-danger rounded-xl border border-[--accent-200] w-fit p-2 hover:bg-[--accent-200] hover:text-black "
+      >
+        {" "}
+        Remove{" "}
+      </button>
+    );
 }
 
 export default Remove
